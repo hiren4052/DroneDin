07fd11309b852b0b585e6c1703d5a903efe54047
Hiren Gabani <hiren_gabani@socialinfotech.com>
2021-02-16 17:18:41 +0530

    blur image

--- app/src/main/java/com/grewon/dronedin/settings/SettingsFragment.kt
+++ app/src/main/java/com/grewon/dronedin/settings/SettingsFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.grewon.dronedin.R
+import jp.wasabeef.glide.transformations.BlurTransformation
 import kotlinx.android.synthetic.main.fragment_settings.*
 
 
@@ -21,7 +24,13 @@ class SettingsFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        initView()
+    }
 
+    private fun initView() {
+        Glide.with(this).load(R.drawable.img_dummy)
+            .apply(RequestOptions.bitmapTransform(BlurTransformation(25, 3)))
+            .into(blur_img_user)
     }
 
 }
